72426d8235ba878dbe16ed404e94046193874993
Sheshko Pavel <derzurb@mail.ru>
2022-02-23 16:55:08 +0300

    small fix

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./Profile.css";
 
 const Profile = () => {
     return (
-        <div className="grid-content">
-            <div className="profile__item__img">
+        <div className="grid__profile">
+            <div className="profile__img">
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
                     alt="Content image"/>
