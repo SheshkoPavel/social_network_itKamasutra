585df241e58930f32fd8fe4fea87b3dd17499c2a
Sheshko Pavel <derzurb@mail.ru>
2022-02-25 17:29:51 +0300

    2022.02.25_Routing in React all pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Test_Input_ClassComponent from "./components/Test_Input_ClassComponent";
 import TestInputF from "./components/Test_Input_F";
 import Dialogs from "./components/Dialogs/Dialogs";
 import {Routes, Route, Link} from 'react-router-dom'
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
 
 function App() {
@@ -19,6 +22,9 @@ function App() {
                 <Routes>
                     <Route path="profile" element={<Profile/>} />
                     <Route path="dialogs" element={<Dialogs/>} />
+                    <Route path="news" element={<News/>} />
+                    <Route path='music' element={<Music/>} />
+                    <Route path='settings' element={<Settings/>} />
                     <Route path="*" element={<Profile/>} />
                 </Routes>
                 {/*<Profile/>*/}
