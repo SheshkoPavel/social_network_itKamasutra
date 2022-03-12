699c9986aeee80596f29531e30fd762dcc6060c8
Sheshko Pavel <derzurb@mail.ru>
2022-03-12 17:54:59 +0300

    2022.03.12_Start creating dispatch function

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,23 +1,23 @@
 let store = {
-    _state : {
+    _state: {
         profilePage: {
-            posts : [
-                {id: 1, message: "Privet volchara", likesCount: 10} ,
+            posts: [
+                {id: 1, message: "Privet volchara", likesCount: 10},
                 {id: 2, message: "Нормально ты заряжаешь", likesCount: 3},
                 {id: 3, message: "Прокинул через пропсы", likesCount: 7}
             ],
             newPostText: 'Hello'
         },
         dialogPage: {
-            dialogs : [
+            dialogs: [
                 {id: 1, name: "Paul"},
-                {id: 2,name: "Den"},
-                {id: 3,name: "Petr"},
+                {id: 2, name: "Den"},
+                {id: 3, name: "Petr"},
                 {id: 4, name: "Viktor"},
                 {id: 5, name: "Kate"},
                 {id: 6, name: "July"}
             ],
-            messages : [
+            messages: [
                 {id: 1, message: "Hi"},
                 {id: 2, message: "Hello"},
                 {id: 3, message: "How are you?"},
@@ -25,15 +25,20 @@ let store = {
                 {id: 5, message: "Yo-yo"}
             ]
         },
-        sidebar : {}
+        sidebar: {}
     },
     _callSubscriber() {
         console.log('state changed')
     },
-    getState () {
-       return this._state;
+
+    getState() {
+        return this._state;
     },
-    addPost () {
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
+
+    addPost() {
         let newPost = {
             id: 5,
             message: this._state.profilePage.newPostText,
@@ -43,14 +48,26 @@ let store = {
         this._state.profilePage.newPostText = '';
         this._callSubscriber(this._state, this.addPost, this.updateNewPostText);
     },
-    updateNewPostText (newText) {
+    updateNewPostText(newText) {
         this._state.profilePage.newPostText = newText;
         this._callSubscriber(this._state, this.addPost, this.updateNewPostText);
     },
-    subscribe (observer) {
-        this._callSubscriber = observer;
-    }
 
+    dispatch(action) {      // { type : 'ADD_POST' }
+        if (action.type === 'ADD_POST') {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = '';
+            this._callSubscriber(this._state, this.addPost, this.updateNewPostText);
+        } else if (action.type === 'UPDATE_NEW_POST_TEXT') {
+            this._state.profilePage.newPostText = action.newText;
+            this._callSubscriber(this._state, this.addPost, this.updateNewPostText);
+        }
+    }
 }
 
 
