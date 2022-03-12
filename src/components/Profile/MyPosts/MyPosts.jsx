1d05529361fb42677c9e68b54f5ec92fb2e22d74
Sheshko Pavel <derzurb@mail.ru>
2022-03-13 00:07:52 +0300

    2022.03.13_global_variables_in_state.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,22 @@
 import React, {useRef} from 'react';
 import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
+
+
 
 const MyPosts = (props) => {
 
     let postElements = props.posts.map((post) => (<Post message={post.message} likes={post.likesCount} />))
 
     const newPostElement = useRef(null);
+
     const addPost = () => {
-        props.dispatch( {type: 'ADD_POST'} );
+        props.dispatch( addPostActionCreator() );
     }
     const onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = {type : 'UPDATE_NEW_POST_TEXT', newText: text };
+        let action = updateNewPostTextActionCreator(text);
         props.dispatch( action );
     }
 
