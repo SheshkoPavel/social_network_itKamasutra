b3141409c8bf58f24e04595b32a6a47a0cb58238
Sheshko Pavel <derzurb@mail.ru>
2022-03-19 17:01:35 +0300

    2022.03.19 api.js, instance of axios.create() that contain baseURL, withCredentials, headers

---
+++ src/api/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "API-KEY" : "f8ad98d1-7488-475c-a890-9534f68a3e9c"
+    }
+});
+
+export const usersAPI = {
+    getUsers (currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`,)
+            .then(response => response.data);
+    }
+}
+
+
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -8,38 +8,29 @@ import {
     setUsers,
     toggleIsFetching
 } from "../../redux/usersReducer";
-import axios from "axios";
 import Users from "./Users";
 import Preloader from "../Common/Preloader/Preloader";
+import {usersAPI} from "../../api/api";
+
 
 class UsersContainer extends React.Component {
     componentDidMount() {
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-            withCredentials: true,
-            headers: {
-                "API-KEY" : "f8ad98d1-7488-475c-a890-9534f68a3e9c"
-            }
-        })
-            .then(response => {
+
+        usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then( data => {
                 this.props.toggleIsFetching(false);
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
+                this.props.setUsers(data.items);
+                this.props.setTotalUsersCount(data.totalCount);
             });
     }
 
     onPageChanged = (pageNumber) => {
         this.props.setCurrentPage(pageNumber);
         this.props.toggleIsFetching(true);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`, {
-            withCredentials: true,
-            headers: {
-                "API-KEY" : "f8ad98d1-7488-475c-a890-9534f68a3e9c"
-            }
-        })
-            .then(response => {
+
+        usersAPI.getUsers(pageNumber, this.props.pageSize).then( data => {
                 this.props.toggleIsFetching(false);
-                this.props.setUsers(response.data.items)
+                this.props.setUsers(data.items)
             });
     }
 
