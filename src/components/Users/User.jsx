22640e7871087de40aeef105f5500e54a6c2f247
Sheshko Pavel <derzurb@mail.ru>
2022-03-28 14:11:14 +0300

    2022.03.28 refactor paginator and Users.jsx page

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import classes from "./Users.module.css";
+import userAvatar from "../../assets/images/cat_ava.jpg";
+
+const User = (props) => {
+    return (
+        <div>
+            <div>
+                <span>
+                        <div>
+                            <NavLink to={'/profile/' + props.user.id}>
+                            <img
+                                className={classes.avatar}
+                                src={
+                                    props.user.photos.small != null
+                                        ? props.user.photos.small
+                                        : userAvatar
+                                } alt="user avatar"/>
+                            </NavLink>
+                        </div>
+
+                        <div>
+                            {props.user.followed
+                                ? <button disabled={props.isFollowingInProgress.some(id => id === props.user.id)}
+                                          onClick={() => {
+                                              props.unFollow(props.user.id)
+                                          }
+                                          }>
+                                    Unfollow</button>
+
+                                :
+                                <button disabled={props.isFollowingInProgress.some(id => id === props.user.id)}
+                                    onClick={() => {
+                                     props.follow(props.user.id)
+                                }
+                                }> Follow </button>
+                            }
+
+                        </div>
+                </span>
+                <span>
+                        <span>
+                            <div>
+                                {props.user.name}
+                            </div>
+                            <div>
+                                {props.user.status}
+                            </div>
+                        </span>
+                        <span>
+                            <div>
+                                {"el.location.country"}
+                            </div>
+                            <div>
+                                {"el.location.city"}
+                            </div>
+                        </span>
+
+                    </span>
+            </div>
+
+        </div>
+    );
+};
+
+export default User;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,78 +1,18 @@
 import React from 'react';
-import classes from "./Users.module.css";
-import userAvatar from "../../assets/images/cat_ava.jpg";
-import {NavLink} from "react-router-dom";
+import Paginator from "../Common/Paginator/Paginator";
+import User from "./User";
 
 const Users = (props) => {
-
-    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i);
-    }
-
     return (
         <div>
-
-            <div>      {/* this was like className={props.currentPage === p && classes.selectedPage}*/}
-                { pages.map(p => {
-                    return <span className={props.currentPage === p ? classes.selectedPage : undefined}
-                                 onClick={(e)=> {props.onPageChanged(p); }} >{p}</span>
-                })}
-
+            <Paginator totalUsersCount={props.totalUsersCount} pageSize={props.pageSize}
+                       currentPage={props.currentPage} onPageChanged={props.onPageChanged}/>
+            <div>
+                {props.users.map((el) => <User user={el} key={el.id} isFollowingInProgress={props.isFollowingInProgress}
+                                               follow={props.follow} unFollow={props.unFollow}/>
+                )
+                }
             </div>
-
-            {
-                props.users.map((el) => <div key={el.id}>
-                    <span>
-                        <div>
-                            <NavLink to={'/profile/' + el.id }>
-                            <img
-                                className={classes.avatar}
-                                src={
-                                    el.photos.small != null
-                                        ? el.photos.small
-                                        : userAvatar
-                                } alt="user avatar"/>
-                            </NavLink>
-                        </div>
-
-                        <div>
-                            {el.followed
-                                ? <button disabled={props.isFollowingInProgress.some(id => id === el.id)}
-                                          onClick={() => { props.unFollow(el.id) }
-                                }>
-                                    Unfollow</button>
-
-                                : <button disabled={props.isFollowingInProgress.some(id => id === el.id)} onClick={() => {
-                                    props.follow(el.id)
-                                    }
-                                }> Follow </button>
-                            }
-
-                        </div>
-                    </span>
-                    <span>
-                        <span>
-                            <div>
-                                {el.name}
-                            </div>
-                            <div>
-                                {el.status}
-                            </div>
-                        </span>
-                        <span>
-                            <div>
-                                {"el.location.country"}
-                            </div>
-                            <div>
-                                {"el.location.city"}
-                            </div>
-                        </span>
-
-                    </span>
-                </div>)
-            }
         </div>
     );
 };
