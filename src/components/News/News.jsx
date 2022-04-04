e4a3ea0627fd3c479a876b327baa657833a886df
Sheshko Pavel <derzurb@mail.ru>
2022-04-05 00:12:50 +0300

    05.04.2022 Last version with TypeScript

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -3,10 +3,9 @@ import axios from "axios";
 import NewsItem from "./NewsItem";
 
 
-
 const News = (props) => {
 
-    const [news, setNews] = useState([])
+    const [news, setNews] = useState([{id: 0, newsText: 'Здесь может быть ваша реклама', imageURL: 'https://r7.pngegg.com/path/452/495/745/react-javascript-angularjs-ionic-github-e09b123ee65b250eca5b7fdc0c317bf4.png'}])
 
     async function getNews() {
        const response = await axios.get("http://localhost:5000/news", {withCredentials: true});
