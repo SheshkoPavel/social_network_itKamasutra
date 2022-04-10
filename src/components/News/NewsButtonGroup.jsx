4b971c45084d07fd66ef065211a11a0de3fe6f47
Sheshko Pavel <derzurb@mail.ru>
2022-04-10 13:31:41 +0300

    10.04.2022 Move logic code from NewsItem.jsx to NewsButtonGroup.jsx

---
+++ src/components/News/NewsButtonGroup.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {ThemeProvider} from "@emotion/react";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {deleteNewsPostThunk, updateNewsPostTextThunk} from "../../redux/newsReducer";
+import {useDispatch} from "react-redux";
+import {createTheme} from "@mui/material/styles";
+import {ButtonGroup} from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import SendIcon from "@mui/icons-material/Send";
+
+const NewsButtonGroup = (props) => {
+    const dispatch = useDispatch()
+
+    const theme = createTheme({
+        palette: {
+            myColorGroup: {
+                main: '#72648b',
+                contrastText: '#fff'
+            },
+        },
+    });
+
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                <ButtonGroup variant="outlined" size="small" aria-label="outlined button group">
+                    <Button startIcon={<EditIcon />}
+                            color={"myColorGroup"}
+                            onClick={()=> {props.setEditMode(true)}}
+                    >
+                        edit
+                    </Button>
+                    <Button startIcon={<DeleteIcon />}
+                            color={"myColorGroup"}
+                            onClick={()=> {dispatch(deleteNewsPostThunk(props.id))}}  >
+                        delete
+                    </Button>
+                    {props.editMode === true ? <Button onClick={() => {
+
+                            dispatch(updateNewsPostTextThunk({updateId: props.id, newNewsText: props.editText}))
+                            props.setEditMode(false);
+                        }}
+                                                 startIcon={<SendIcon />}
+                                                 color={"myColorGroup"}
+
+                        >
+                            ok
+                        </Button>
+                        : null}
+                </ButtonGroup>
+            </ThemeProvider>
+        </div>
+    );
+};
+
+export default NewsButtonGroup;
--- src/components/News/NewsItem.jsx
+++ src/components/News/NewsItem.jsx
@@ -1,29 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import './NewsItem.scss'
-import NewsDeleteButton from "./NewsDeleteButton";
-import {useDispatch} from "react-redux";
-import {createTheme} from "@mui/material/styles";
-import {ThemeProvider} from "@emotion/react";
-import {ButtonGroup} from "@mui/material";
-import Button from "@mui/material/Button";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from '@mui/icons-material/Send';
-import {deleteNewsPostThunk, updateNewsPostTextThunk} from "../../redux/newsReducer";
-
+import NewsButtonGroup from "./NewsButtonGroup";
 
 const NewsItem = (props) => {
-
-    const dispatch = useDispatch()
-
-    const theme = createTheme({
-        palette: {
-            myColorGroup: {
-                main: '#72648b',
-                contrastText: '#fff'
-            },
-        },
-    });
 
     const [editMode, setEditMode] = useState(false)
     const [editText, setEditText] = useState(props.newsText);
@@ -48,35 +27,12 @@ const NewsItem = (props) => {
                                     : <input type={"text"} value={editText} onChange={onNewsTextChange} />
 
                 }
-                <div>
-                    <ThemeProvider theme={theme}>
-                        <ButtonGroup variant="outlined" size="small" aria-label="outlined button group">
-                            <Button startIcon={<EditIcon />}
-                                    color={"myColorGroup"}
-                                    onClick={()=> {setEditMode(true)}}
-                            >
-                                edit
-                            </Button>
-                            <Button startIcon={<DeleteIcon />}
-                                    color={"myColorGroup"}
-                                    onClick={()=> {dispatch(deleteNewsPostThunk(props.id))}}  >
-                                delete
-                            </Button>
-                            {editMode === true ? <Button onClick={() => {
-
-                                dispatch(updateNewsPostTextThunk({updateId: props.id, newNewsText: editText}))
-                                setEditMode(false);
-                            }}
-                                                         startIcon={<SendIcon />}
-                                                         color={"myColorGroup"}
-
-                                                >
-                                ok
-                            </Button>
-                                : null}
-                        </ButtonGroup>
-                    </ThemeProvider>
-                </div>
+                <NewsButtonGroup editMode={editMode}
+                                 setEditMode={setEditMode}
+                                 editText={editText}
+                                 setEditText={setEditText}
+                                 id={props.id}
+                />
 
             </div>
         </div>
