53838ca61e7d068730c8ab7a28237793f640d8b4
Sheshko Pavel <derzurb@mail.ru>
2022-04-12 00:47:33 +0300

    12.04.2022 small visual fix

--- src/components/News/AddNewsForm.jsx
+++ src/components/News/AddNewsForm.jsx
@@ -15,10 +15,14 @@ const AddNewsForm = () => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <textarea style={{width: 300, height: 50}} placeholder="Write text for new News" {...register("newsText", {required: true})} />
+            <textarea style={{width: 300, height: 70, resize: "none"}}
+                      placeholder="Write text for new News"
+                      {...register("newsText", {required: true})} />
             <div style={{color: "rebeccapurple"}}>{errors.newsText?.type === 'required' && "You must write something"}</div>
 
-            <input style={{width: 300}} placeholder='Insert URL' {...register('imageURL', {required: true})}  />
+            <input style={{width: 300}}
+                   placeholder='Insert URL'
+                   {...register('imageURL', {required: true})}  />
             <div style={{color: "rebeccapurple"}}>{errors.imageURL?.type === 'required' && "You must write something"}</div>
 
             <input type="submit" />
