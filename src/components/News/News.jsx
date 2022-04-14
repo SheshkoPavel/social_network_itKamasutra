a0c2101e58f67a063a4e0b262477ac861bd7c353
Sheshko Pavel <derzurb@mail.ru>
2022-04-14 14:40:52 +0300

    14.04.2022 Replace comment

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -7,9 +7,16 @@ import Button from "@mui/material/Button";
 import ImportExportIcon from '@mui/icons-material/ImportExport';
 
 const News = (props) => {
+    /*    Local server request for take news
+        const [news, setNews] = useState([{id: 0, newsText: 'Здесь может быть ваша реклама', imageURL: 'https://r7.pngegg.com/path/452/495/745/react-javascript-angularjs-ionic-github-e09b123ee65b250eca5b7fdc0c317bf4.png'}])
 
+        async function getNews() {
+            const response = await axios.get("http://localhost:5000/news", {withCredentials: true});
+            setNews(response.data);
+        }
+
+        useEffect(() => {getNews()},[])*/
     const dispatch = useDispatch()
-
 
     useEffect(() => {
         dispatch(getNewsThunk())
@@ -26,16 +33,6 @@ const News = (props) => {
         dispatch(getNewsThunk());
         setState(false);
     }
-
-/*    Local server request for take news
-    const [news, setNews] = useState([{id: 0, newsText: 'Здесь может быть ваша реклама', imageURL: 'https://r7.pngegg.com/path/452/495/745/react-javascript-angularjs-ionic-github-e09b123ee65b250eca5b7fdc0c317bf4.png'}])
-
-    async function getNews() {
-        const response = await axios.get("http://localhost:5000/news", {withCredentials: true});
-        setNews(response.data);
-    }
-
-    useEffect(() => {getNews()},[])*/
 
     let newsElements = news.map((n, index) => <NewsItem key={n.id} id={n.id}
                                                         newsText={n.newsText} imageURL={n.imageURL}
