89fba1c805020490713e5de9ef735b799bf8d501
Sheshko Pavel <derzurb@mail.ru>
2022-04-20 14:32:54 +0300

    20.04.2022 textarea on add post layout change

--- src/components/Profile/AddPostForm.jsx
+++ src/components/Profile/AddPostForm.jsx
@@ -12,7 +12,11 @@ const AddPostForm = (props) => {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <textarea placeholder="Write smth" {...register("newPostText", {required: true})} />
+            <textarea placeholder="Write smth"
+                      {...register("newPostText", {required: true})}
+                style={{resize: 'none', width: 300, height: 80}}
+
+            />
             <div style={{color: "rebeccapurple"}}>{errors.newPostText?.type === 'required' && "You must write something"}</div>
 
             <input type="submit" />
